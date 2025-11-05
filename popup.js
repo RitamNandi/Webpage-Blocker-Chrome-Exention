e0541868724cf560f5aff2f4101c6bd4e11faa87
Ritam Nandi <nandi.ritamnandi@gmail.com>
2025-11-05 17:36:03 -0600

    Popup aesthetic changes

--- popup.js
+++ popup.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const optionsButton = document.getElementById('options-page');
 
     optionsButton.addEventListener('click' , () => {
-        chrome.runtime.optnOptionsPage(); // Chrome API to open the options page
+        chrome.runtime.openOptionsPage(); // Chrome API to open the options page
     });
 
 });
